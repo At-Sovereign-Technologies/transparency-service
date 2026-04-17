24b65066fd9dabe62f8004d59bcf9497eec3fcde
Palmxl <jmartin_sanchez@javeriana.edu.co>
2026-04-16 19:59:18 -0500

    first commit

---
+++ src/main/java/com/electoral/transparency_service/TransparencyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.electoral.transparency_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransparencyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransparencyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/electoral/transparency_service/TransparencyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electoral.transparency_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransparencyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
